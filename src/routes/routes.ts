1f1c22c5d4a96dd439cc3f4627eea647cc43e519
JoaoPaiva <jvlimapaiva@gmail.com>
2022-10-17 15:47:22 -0300

    config prettier

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,6 @@
 import { Request, Response, Router } from "express";
 const routes = Router();
 
-
 //Rota de teste
 routes.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "funcionando" });
