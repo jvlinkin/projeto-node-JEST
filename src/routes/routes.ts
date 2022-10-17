ac7697efd94fe4c5f1008aa717009935218a1283
JoaoPaiva <jvlimapaiva@gmail.com>
2022-10-17 15:37:20 -0300

    config eslint

---
+++ src/routes/routes.ts
@@ -0,0 +1,10 @@
+import { Request, Response, Router } from "express";
+const routes = Router();
+
+
+//Rota de teste
+routes.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "funcionando" });
+});
+
+export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 const app = express();
 const PORT = process.env.PORT || 4333;
+import routes from "./routes/routes";
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "Funcionando." });
-});
+//Rotas
+app.use(routes);
 
 app.listen(PORT, () => {
+  /*Here, you can use to connect do database, cause, if db is not connected,
+   it does not make sense to init the server*/
   console.log(`Server is running on http://localhost:${PORT}`);
 });
